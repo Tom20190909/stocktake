60772059cc58ec2172165ec16358dfc096f2299d
Tom20190909 <262545789@qq.com>
2020-10-24 11:56:15 +0800

    页签显示信息更改

--- stocktake/Inventory.Designer.cs
+++ stocktake/Inventory.Designer.cs
@@ -164,7 +164,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1426, 692);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "制作信息显示";
+            this.tabPage2.Text = "信息显示";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
